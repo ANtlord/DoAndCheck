c7a46a9488df1250e210fff9dc5371791f71e2e4
lantlord <antlord92@gmail.com>
2017-08-31 02:53:32 +0800

    A window title was corrected.

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
+        Form.setWindowTitle(_translate("Form", "Do And Check"))
         __sortingEnabled = self.listWidget.isSortingEnabled()
         self.listWidget.setSortingEnabled(False)
         self.listWidget.setSortingEnabled(__sortingEnabled)
