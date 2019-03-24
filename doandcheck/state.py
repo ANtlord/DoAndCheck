37f8632a7583bb91ba0c52d40fafebcb50f877fe
ANtlord <antlord92@gmail.com>
2019-03-25 05:15:52 +0800

    Unusable prints are removed.

--- doandcheck/state.py
+++ doandcheck/state.py
@@ -4,7 +4,6 @@ class Base:
     __slots__ = '_state', '_widget',
 
     def __init__(self, widget):
-        print('State is ', self.__class__)
         widget._state = self
         self._widget = widget
 
@@ -28,14 +27,11 @@ class Idle(Base):
         Editing(self._widget)
 
     def add(self):
-        print('add')
         #  item = build_list_item()
         self._widget.listWidget.add()
         model = self._widget.listWidget.model()
-        print('self._widget.listWidget.count', self._widget.listWidget.count())
         index = model.index(self._widget.listWidget.count() - 1, 0)
         if index.row() != self._widget.listWidget.count() - 1:
-            print('index.isValid()', index.isValid(), 'row', index.row())
             exit(1)
         self._widget.listWidget.setCurrentIndex(index)
         currentIndex = self._widget.listWidget.currentIndex()
@@ -49,17 +45,12 @@ class Editing(Base):
         super().__init__(widget)
         listWidget = self._widget.listWidget
         if listWidget.count():
-            #  listWidget.editItem(listWidget.currentItem())
-            #  model: TaskModel = listWidget.model()
             index = listWidget.currentIndex()
-            print('edit row', index.row())
             listWidget.openPersistentEditor(index)
             self.resume()
 
     def idle(self):
         listWidget = self._widget.listWidget
-        #  model: TaskModel = listWidget.model()
-        #  index = model.index(model.rowCount() - 1)
         index = listWidget.currentIndex()
         listWidget.closePersistentEditor(index)
         Idle(self._widget)
